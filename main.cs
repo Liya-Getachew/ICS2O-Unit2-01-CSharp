7db90670416d3b2cb2f260f0dd92f1fa6433663e
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-05 18:37:04 +0000

    c# again

--- main.cs
+++ main.cs
@@ -5,10 +5,11 @@
 
 using System;
 
-class Program {
+class Program
+{
     public static void Main(string[] args)
-     {
+    {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("\nDone.");
-     }
+    }
 }
