8a6ac1d5b1c6e3e34ca77d204cdf758237c98173
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-05 18:23:55 +0000

    formatting

--- main.cs
+++ main.cs
@@ -7,8 +7,10 @@ using System;
 
 class Program 
 {
-  public static void Main (string[] args) {
-   Console.WriteLine ("Hello, World!");
-   Console.WriteLine ("\nDone");
-  }
+    public static void Main(string[] args)
+     {
+         Console.WriteLine("Hello, World!");
+
+         Console.WriteLine("\nDone.");
+     }
 }
